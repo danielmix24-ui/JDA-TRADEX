c450eaab3d3ce4fce6c410010f3ffd660da73e8e
jda <danielmix24@gmail.com>
2026-05-19 16:20:04 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbTAoAtNbMtjMzKGxq37h1sgFASM7GgT4",
+  authDomain: "jda-tradex.firebaseapp.com",
+  databaseURL: "https://jda-tradex-default-rtdb.firebaseio.com",
+  projectId: "jda-tradex",
+  storageBucket: "jda-tradex.firebasestorage.app",
+  messagingSenderId: "171375297732",
+  appId: "1:171375297732:web:1075542efff75dd7ce3591"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
